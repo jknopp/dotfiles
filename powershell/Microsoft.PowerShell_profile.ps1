3aaf13d7e2fbce1aaeb81e30d2e04550a47e8628
Jon Knopp <owner@jonknopp.com>
2020-12-01 23:04:06 -0700

    inti

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme pure
+
+If (-Not (Test-Path Variable:PSise)) {
+    Import-Module Get-ChildItemColor
+    Set-Alias l Get-ChildItem -option AllScope
+    Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+}
+
+Set-PSReadlineOption -Color @{
+    "Command" = [ConsoleColor]::Green
+    "Parameter" = [ConsoleColor]::Gray
+    "Operator" = [ConsoleColor]::Magenta
+    "Variable" = [ConsoleColor]::White
+    "String" = [ConsoleColor]::Yellow
+    "Number" = [ConsoleColor]::Blue
+    "Type" = [ConsoleColor]::Cyan
+    "Comment" = [ConsoleColor]::DarkCyan
+}
